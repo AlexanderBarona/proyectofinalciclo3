786f009383d5e83a667164b70c739b5e6e11be62
AlexanderBarona <90266472+AlexanderBarona@users.noreply.github.com>
2021-10-27 21:13:22 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@
 const mongoose= require('mongoose');
 //const URI = process.env.MONGODB_URI;
 
-mongoose.connect("mongodb+srv://adminpd:4142mintic@cluster0.wz9o2.mongodb.net/ProductosGwen") 
+mongoose.connect("mongodb+srv://:@cluster0.wz9o2.mongodb.net/ProductosGwen") 
 //mongoose.connect(URI)
 
 .then(db=> console.log('Conectado a la DB Gwen'))
