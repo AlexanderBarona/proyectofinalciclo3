25b8c479daf025ee1705781cde61d5ae5b973205
AlexanderBarona <90266472+AlexanderBarona@users.noreply.github.com>
2021-10-27 21:27:43 -0500

    Add files via upload

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 
-//require('dotenv').config();
+require('dotenv').config();
 const mongoose= require('mongoose');
-//const URI = process.env.MONGODB_URI;
+const URI = process.env.MONGODB_URI;
 
-mongoose.connect("mongodb+srv://:@cluster0.wz9o2.mongodb.net/ProductosGwen") 
+mongoose.connect(URI) 
 //mongoose.connect(URI)
 
 .then(db=> console.log('Conectado a la DB Gwen'))
